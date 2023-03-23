cdb448d3ed9ac15f957b5751332a2d10ae571026
ignalpha4 <shubhambankar444@gmail.com>
2023-03-24 01:59:29 +0530

    asd

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,7 +1,54 @@
-import React from 'react'
+import React from 'react';
+import { Container,Box,Text,Tab,TabList,TabPanel,TabPanels,Tabs}from '@chakra-ui/react';
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
 
 const Homepage= () => {
-    return <div>Home</div>
+    return <Container maxW="xl" centerContent>    
+
+    <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+        >
+
+        <Text textAlign="center" fontSize={"4xl"} fontFamily="Work sans" color="black">ChatBox</Text>
+
+        
+    </Box>
+
+    <Box bg="white" w="100%" p={4} color="black" borderRadius="lg" borderWidth="1px">
+
+        <Tabs variant='soft-rounded' colorScheme='green'>
+        <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
+        </TabList>
+        <TabPanels>
+            <TabPanel><Login /></TabPanel>
+            
+            <TabPanel><Signup /></TabPanel>
+           
+            
+        </TabPanels>
+        </Tabs>
+
+    </Box>
+
+
+        
+
+
+   
+
+
+
+    </Container>
     
 };
 
