9943bdc892f82626bb9da3f5777bdb2d5e1345af
ignalpha4 <shubhambankar444@gmail.com>
2023-04-22 19:09:45 +0530

    connected db

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,13 @@ const express=require("express");
 const dotenv=require("dotenv");
 
 const { chats } = require("./data/data");
+const connectDB= require("./config/db");
+const colors=require('colors');
 
 
-const app=express();
 dotenv.config();
-
+connectDB();  
+const app=express();
 
 app.get('/',(req,res)=>{
     res.send("API is running");
@@ -23,4 +25,4 @@ app.get('/api/chat',(req,res)=>{
 
 const PORT=process.env.PORT || 8000
 
-app.listen(PORT,console.log("server started on port ${PORT} "));
+app.listen(PORT,console.log("server started on port ${PORT} ".yellow.bold));
